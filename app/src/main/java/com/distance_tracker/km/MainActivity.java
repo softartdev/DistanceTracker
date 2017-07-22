72cfcb9b690ac612490235e48630ad1b6d251316
artur <artik222012@gmail.com>
2017-07-23 03:18:24 +0500

    add AlertDialog

--- app/src/main/java/com/distance_tracker/km/MainActivity.java
+++ app/src/main/java/com/distance_tracker/km/MainActivity.java
@@ -1,22 +1,25 @@
 package com.distance_tracker.km;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, DialogInterface.OnClickListener {
+    Button startTrackingButton, stopTrackingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button startTrackingButton = (Button) findViewById(R.id.start_tracking_button);
+        startTrackingButton = (Button) findViewById(R.id.start_tracking_button);
         startTrackingButton.setOnClickListener(this);
 
-        Button stopTrackingButton = (Button) findViewById(R.id.stop_tracking_button);
+        stopTrackingButton = (Button) findViewById(R.id.stop_tracking_button);
         stopTrackingButton.setOnClickListener(this);
 
         Button showDistanceButton = (Button) findViewById(R.id.show_distance_button);
@@ -30,14 +33,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.start_tracking_button:
+                AlertDialog alertDialog = new AlertDialog.Builder(this)
+                        .setMessage(R.string.location_tracked_message)
+                        .setPositiveButton(android.R.string.ok, this)
+                        .setNegativeButton(android.R.string.cancel, this)
+                        .create();
+                alertDialog.show();
                 break;
             case R.id.stop_tracking_button:
+                startTrackingButton.setVisibility(View.VISIBLE);
+                stopTrackingButton.setVisibility(View.GONE);
                 break;
             case R.id.show_distance_button:
                 startActivity(new Intent(this, DistanceActivity.class));
                 break;
             case R.id.show_location_button:
                 startActivity(new Intent(this, LocationActivity.class));
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        switch (which) {
+            case DialogInterface.BUTTON_POSITIVE:
+                startTrackingButton.setVisibility(View.GONE);
+                stopTrackingButton.setVisibility(View.VISIBLE);
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                dialog.cancel();
                 break;
         }
     }
